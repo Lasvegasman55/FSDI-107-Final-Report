1c021dfd392e66faf7ede6a48e4876b2140d9067
Lasvegasman55 <ftwalton31@yahoo.com>
2024-12-20 05:32:11 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import pymongo
 import certifi
+import os  # Added for environment variables
 
-con_string = mongodb+srv://coolcatt28:<db_coolcatt28>@cluster0.kldjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-client = pymango.MangoClient(con_string, tlsCaFile = certif.where())
+# Better to get connection string from environment variable
+con_string = "mongodb+srv://coolcatt28:<db_coolcatt28>@cluster0.kldjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = pymongo.MongoClient(con_string, tlsCAFile=certifi.where())
 db = client.get_database("Tacos")
